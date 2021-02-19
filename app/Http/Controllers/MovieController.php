fa7d1fd340e3bbb3bb596b1ec7b7bb0621f2e786
Manuel <carlosd99m@gmail.com>
2021-02-19 12:59:48 -0600

    Get de todas las peliculas

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -13,7 +14,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        return Movie::all();
     }
 
     /**
